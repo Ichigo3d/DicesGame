5ec388db8e8d0e4e9461296abc82ab206e988ddb
Matvei Iliunov <37351864+Ichigo3d@users.noreply.github.com>
2021-03-31 05:08:28 -0400

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,11 @@
+class Dice:
+    SIDE1 = 1
+    SIDE2 = 2
+    SIDE3 = 3
+    SIDE4 = 4
+    SIDE5 = 5
+    SIDE6 = 6
+
+
+
+
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+
+    def __init__(self, name, total_score=0):
+        self.name = name
+        self.total_score = total_score
